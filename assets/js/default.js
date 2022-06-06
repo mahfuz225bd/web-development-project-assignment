edcdfab182d72c8d8de7f939e0f32e594fda26ff
mahfuz <mahfuz01bd@gmail.com>
2022-06-06 18:16:46 +0600

    social links

--- assets/js/default.js
+++ assets/js/default.js
@@ -4,12 +4,6 @@ import { animatedTypingH2 } from './animatedTypingH2.js';
 // Enable `include-html` attribute for including html file
 includeHTML();
 
-window.addEventListener('load', () => {
-	// const headerHeight = document.querySelector('header').offsetHeight;
-	// const main = document.querySelector('main');
-	// main.style.paddingTop = headerHeight + 'px';
-});
-
 document.addEventListener('DOMContentLoaded', () => {
 	// Animated typing
 	animatedTypingH2('<Full Stack Developer>', 100);
