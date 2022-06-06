f881847587a5222084b30c71bc181fe3f0624f11
mahfuz <mahfuz01bd@gmail.com>
2022-06-06 18:21:51 +0600

    toggle social bar issue solve

--- assets/js/default.js
+++ assets/js/default.js
@@ -4,7 +4,19 @@ import { animatedTypingH2 } from './animatedTypingH2.js';
 // Enable `include-html` attribute for including html file
 includeHTML();
 
-document.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('load', () => {
 	// Animated typing
 	animatedTypingH2('<Full Stack Developer>', 100);
+
+	// Toggle Social Bar
+	const toggleSocialBar = document.getElementById('toggleSocialBar');
+	toggleSocialBar.addEventListener('click', () => {
+		if (socialBar.className === 'social-icon-bar') {
+			socialBar.className = 'social-icon-bar res-show';
+			toggleSocialBar.innerHTML = 'X';
+		} else {
+			socialBar.className = 'social-icon-bar';
+			toggleSocialBar.innerHTML = 'Follow me';
+		}
+	});
 });
