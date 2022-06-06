248926420c15f7e8efc6c985809b80c812161863
mahfuz <mahfuz01bd@gmail.com>
2022-06-06 14:28:54 +0600

    file structure organized

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,10 +1,10 @@
 import { includeHTML } from './includeHTML.js';
 import { animatedTypingH2 } from './animatedTypingH2.js';
 
-// Enable include-html="" for including html file
+// Enable `include-html` attribute for including html file
 includeHTML();
 
 document.addEventListener('DOMContentLoaded', () => {
 	// Animated typing
-	animatedTypingH2('<Full Stack Developer and Freelancer>', 100);
+	animatedTypingH2('<Full Stack Developer>', 100);
 });
