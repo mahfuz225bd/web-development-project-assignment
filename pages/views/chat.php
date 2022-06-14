18064b85adabc50459948c0bd5f88023c6b4c204
mahfuz <mahfuz01bd@gmail.com>
2022-06-14 06:58:58 +0600

    added chatbot form inputs

--- pages/views/chat.php
+++ pages/views/chat.php
@@ -8,7 +8,12 @@
   <form action="#" class="form-container">
     <h1>Chat</h1>
 
-    <label for="msg"><b>Message</b></label>
+    <input type="text" name="name" placeholder="Name *" autocomplete="off" required />
+
+    <input type="email" name="email" placeholder="Email Address" autocomplete="off" />
+
+    <input type="text" name="phone" placeholder="Phone Number" autocomplete="off" />
+
     <textarea placeholder="Type message.." name="msg" required></textarea>
 
     <button type="submit" class="btn btn-primary">Send</button>
